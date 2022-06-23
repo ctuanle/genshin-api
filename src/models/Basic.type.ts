d78d8d6c5163ab752063fe58eba3fc4044f70e4b
LE Cong Tuan <congtuanle23@gmail.com>
2022-06-23 10:44:37 +0200

    refactor(BasicType): using enum instead of literal type

--- src/models/Basic.type.ts
+++ src/models/Basic.type.ts
@@ -1,52 +1,71 @@
-export type Region =
-  | 'Mondstadt'
-  | 'Liyue'
-  | 'Inazuma'
-  | 'Sumeru'
-  | 'Fontaine'
-  | 'Natlan'
-  | 'Snezhnaya'
-  | "Khaenri'ah"
-  | 'Unknown';
+export enum Region {
+  Mondstadt = 'Mondstadt',
+  Liyue = 'Liyue',
+  Inazuma = 'Inazuma',
+  Sumeru = 'Sumeru',
+  Fontaine = 'Fontaine',
+  Natlan = 'Natlan',
+  Snezhnaya = 'Snezhnaya',
+  Khaenriah = "Khaenri'ah",
+  Unknown = 'Unknown',
+}
 
-export type ModelType =
-  | 'Tall male'
-  | 'Tall female'
-  | 'Medium male'
-  | 'Medium female'
-  | 'Short female';
+export enum ModelType {
+  TallMale = 'Tall male',
+  TallFemale = 'Tall female',
+  MediumMale = 'Medium male',
+  MediumFemale = 'Medium female',
+  ShortFemale = 'Short female',
+}
 
-export type WeaponType = 'Sword' | 'Polearm' | 'Bow' | 'Claymore' | 'Catalyst';
+export enum WeaponType {
+  Sword = 'Sword',
+  Polearm = 'Polearm',
+  Bow = 'Bow',
+  Claymore = 'Claymore',
+  Catalyst = 'Catalyst',
+}
 
-export type VisionType =
-  | 'Pyro'
-  | 'Cryo'
-  | 'Hydro'
-  | 'Dendro'
-  | 'Anemo'
-  | 'Electro'
-  | 'Geo'
-  | 'None';
+export enum VisionType {
+  Pyro = 'Pyro',
+  Cryo = 'Cryo',
+  Hydro = 'Hydro',
+  Dendro = 'Dendro',
+  Anemo = 'Anemo',
+  Electro = 'Electro',
+  Geo = 'Geo',
+  None = 'None',
+}
 
-export type RarityType = '4_star' | '5_star';
+export enum RarityType {
+  FourStart = '4_star',
+  FiveStart = '5_start',
+}
 
-export type Version =
-  | 'v1.0'
-  | 'v1.1'
-  | 'v1.2'
-  | 'v1.3'
-  | 'v1.4'
-  | 'v1.5'
-  | 'v1.6'
-  | 'v2.0'
-  | 'v2.1'
-  | 'v2.2'
-  | 'v2.3'
-  | 'v2.4'
-  | 'v2.5'
-  | 'v2.6'
-  | 'v2.7'
-  | 'upcoming';
+export enum Version {
+  v10 = 'v1.0',
+  v11 = 'v1.1',
+  v12 = 'v1.2',
+  v13 = 'v1.3',
+  v14 = 'v1.4',
+  v15 = 'v1.5',
+  v16 = 'v1.6',
+  v20 = 'v2.0',
+  v21 = 'v2.1',
+  v22 = 'v2.2',
+  v23 = 'v2.3',
+  v24 = 'v2.4',
+  v25 = 'v2.5',
+  v26 = 'v2.6',
+  v27 = 'v2.7',
+  upcoming = 'upcoming',
+}
+
+export enum BannerType {
+  Character = 'Character',
+  Weapon = 'Weapon',
+  Permanent = 'Permanent',
+}
 
 export type VoiceActorsType = {
   English: string;
@@ -54,5 +73,3 @@ export type VoiceActorsType = {
   Japanese: string;
   Korean: string;
 };
-
-export type BannerType = 'Character' | 'Weapon' | 'Permanent';
